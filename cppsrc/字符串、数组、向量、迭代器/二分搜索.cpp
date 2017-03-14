569355a22ca24d8bcbc4ca4ac5c4d0231f25c371
timeloveboy <734991033@qq.com>
2017-03-14 16:06:32 +0800

    !~

--- cppsrc/字符串、数组、向量、迭代器/二分搜索.cpp
+++ cppsrc/字符串、数组、向量、迭代器/二分搜索.cpp
@@ -1,6 +1,41 @@
 #include<iostream>
-using namespace std;
+#include<vector>
 
-main(){
-        vector<int> v5={1,3,234,31,5,10086};
+
+using namespace std;
+using std::vector;
+
+int find(vector<int> src,int target){
+    int beg = 0;
+    int end = src.size();
+     int  mid = beg + (end - beg) / 2;
+
+    while (mid != end && src[mid] != target)
+    {
+        if (target < src[mid])
+        {
+            end = mid;
+        }
+        else
+            {
+            beg = mid + 1;
+        }
+        mid = beg + (end - beg) / 2;
+    }
+    if(src[mid] != target){
+    return -1;
+    }else{
+     return mid;
+     }
+
+}
+
+int main()
+{
+    vector<int> text{54,87,575,4587,54245,54255,58655,59654,60000,60555,65588};
+    int keyindex=find(text,54255);
+    cout<<"position:" <<keyindex;
+
+    cin.get();
+    return 0;
 }
